a9bcf9b7175960e7afe905a63a89ec37a7374e20
Natan Roberto Xavier <121309547+natanrobertoxavier@users.noreply.github.com>
2023-04-06 19:38:10 -0300

    Sync Communication

---
+++ S21_ErudioMicroservices.NET6-SyncCommunication/GeekShopping/GeekShopping.OrderAPI/Repository/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using GeekShopping.OrderApi.Model;
+
+namespace GeekShopping.OrderAPI.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<bool> AddOrder(OrderHeader header);
+        Task UpdateOrderPaymentStatus(long orderHeaderId, bool paid);
+    }
+}
